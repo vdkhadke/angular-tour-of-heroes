29caa0f7d867c419ba77ddb6000e96aab889d665
Viplav <1767617+vdkhadke@users.noreply.github.com>
2020-07-08 00:00:35 -0400

    Part 6: Add Search Component and finish the tour.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,6 +66,20 @@ export class HeroService {
     );
   }
 
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+         this.log(`found heroes matching "${term}"`) :
+         this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   /**
  * Handle Http operation that failed.
  * Let the app continue.
